61b89e8c3bec08b3cd781fea582485f328b11227
Berthet <lu.berthet@gmail.com>
2025-04-08 13:29:34 +0200

    fix animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import {MainMotion} from "./components/MotionComponent/MainMotion.tsx";
 import {DotLottieReact} from '@lottiefiles/dotlottie-react';
+import {MainMotion} from "./components/MotionComponent/MainMotion.tsx";
 
 function App() {
 
     return (
         <MainMotion className="text-white flex flex-col gap-10 items-center" delay={0.1}>
-            <h1 className="text-3xl font-bold">Bienvenue sur mon site d'invitation à la soirée de Noël !</h1>
+            <h1 className="text-2xl font-bold">Bienvenue sur mon site d'invitation à la soirée de Noël !</h1>
             <p>Parce que oui Noël ça se prépare en avance !</p>
 
-            <DotLottieReact
-                src="https://lottie.host/2a32727d-0721-49d3-b06f-12440a5a2738/po9i0uwPj5.lottie"
-                loop
-                autoplay
-            />
+            <div className="h-90">
+                <DotLottieReact
+                    src="https://lottie.host/2a32727d-0721-49d3-b06f-12440a5a2738/po9i0uwPj5.lottie"
+                    loop
+                    autoplay
+                />
+            </div>
         </MainMotion>
     )
 }
